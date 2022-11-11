238cdbf97cf0a6871a980112776d6d14cb354d4c
Александр Биктеев <bikteev@mail.ru>
2022-11-11 17:54:56 +0600

    add spring framework setup ui

--- AnimationWitchSpring/ViewController.swift
+++ AnimationWitchSpring/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var springAnimationView: UIView!
+    
+    @IBOutlet var presetLabel: UILabel!
+    @IBOutlet var curveLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
+    @IBOutlet var durationLabel: UILabel!
+    @IBOutlet var delayLabel: UILabel!
+    
+    @IBOutlet var runButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func runActionButton(_ sender: Any) {
+    }
+    
 
 }
 
